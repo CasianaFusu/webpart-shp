45a5fbe5e887584bc44e6055175b312ed7f9eb08
SharePoint Student <student@CONTOSOLTD.LOCAL>
2022-09-03 19:51:04 +0200

    search

--- src/webparts/navigareCems/my-script.js
+++ src/webparts/navigareCems/my-script.js
@@ -3,6 +3,7 @@ window.myFunction = function() {
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById('myInput');
   filter = input.value.toUpperCase();
+
   ul = document.getElementById("myUL");
   li = ul.getElementsByTagName('li');
 
