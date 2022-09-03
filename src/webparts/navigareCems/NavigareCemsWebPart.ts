fb83a888bda640d2b45526253e63772c7b4e717f
SharePoint Student <student@CONTOSOLTD.LOCAL>
2022-09-03 18:09:52 +0200

    accordion

--- src/webparts/navigareCems/NavigareCemsWebPart.ts
+++ src/webparts/navigareCems/NavigareCemsWebPart.ts
@@ -9,28 +9,84 @@ import { escape } from '@microsoft/sp-lodash-subset';
 import styles from './NavigareCemsWebPart.module.scss';
 import * as strings from 'NavigareCemsWebPartStrings';
 
+import * as jQuery from 'jquery';
+import 'jqueryui';
+
+import { SPComponentLoader } from '@microsoft/sp-loader';
+
 export interface INavigareCemsWebPartProps {
   description: string;
 }
 
 export default class NavigareCemsWebPart extends BaseClientSideWebPart<INavigareCemsWebPartProps> {
 
+  public constructor() {
+    super();
+  
+    SPComponentLoader.loadCss('//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css');
+  }
+
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.navigareCems }">
-        <div class="${ styles.container }">
-          <div class="${ styles.row }">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">CEMS</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>`;
+    <div class="accordion">
+      <h3>Section 1</h3>
+      <div>
+          <p>
+          Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+          ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+          amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+          odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+          </p>
+      </div>
+      <h3>Section 2</h3>
+      <div>
+          <p>
+          Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+          purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+          velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+          suscipit faucibus urna.
+          </p>
+      </div>
+      <h3>Section 3</h3>
+      <div>
+          <p>
+          Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+          Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+          ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+          lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+          </p>
+          <ul>
+          <li>List item one</li>
+          <li>List item two</li>
+          <li>List item three</li>
+          </ul>
+      </div>
+      <h3>Section 4</h3>
+      <div>
+          <p>
+          Cras dictum. Pellentesque habitant morbi tristique senectus et netus
+          et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
+          faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
+          mauris vel est.
+          </p>
+          <p>
+          Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos.
+          </p>
+      </div>
+  </div>`;
+
+  const accordionOptions: JQueryUI.AccordionOptions = {
+    animate: true,
+    collapsible: false,
+    icons: {
+      header: 'ui-icon-circle-arrow-e',
+      activeHeader: 'ui-icon-circle-arrow-s'
+    }
+  };
+
+  jQuery('.accordion', this.domElement).accordion(accordionOptions);
   }
 
   protected get dataVersion(): Version {
