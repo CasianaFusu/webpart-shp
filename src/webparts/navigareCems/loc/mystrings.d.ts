159adfbae231c2eb586896f93cf3a52ac2c8edc7
SharePoint Student <student@CONTOSOLTD.LOCAL>
2022-09-03 17:28:19 +0200

    first commit

---
+++ src/webparts/navigareCems/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INavigareCemsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'NavigareCemsWebPartStrings' {
+  const strings: INavigareCemsWebPartStrings;
+  export = strings;
+}
